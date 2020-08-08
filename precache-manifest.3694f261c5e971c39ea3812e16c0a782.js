68d209460d62e5f28fae26de6ca2415504e3e6eb
christopherorea <c28_09_98@hotmail.com>
2020-08-08 18:00:37 -0500

    Updates

---
+++ precache-manifest.3694f261c5e971c39ea3812e16c0a782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7682ff1addd1fa686978a39bf01c96d2",
+    "url": "/React-Video/index.html"
+  },
+  {
+    "revision": "f518e470f37802e12723",
+    "url": "/React-Video/static/css/main.8cc11b67.chunk.css"
+  },
+  {
+    "revision": "84e2392488ef3d72ca34",
+    "url": "/React-Video/static/js/2.2bb4582d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Video/static/js/2.2bb4582d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f518e470f37802e12723",
+    "url": "/React-Video/static/js/main.932eeaa7.chunk.js"
+  },
+  {
+    "revision": "1f68e4a15b7f00797660",
+    "url": "/React-Video/static/js/runtime-main.20aa0398.js"
+  }
+]);
